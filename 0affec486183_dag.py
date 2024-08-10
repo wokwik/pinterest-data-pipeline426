dba1f3fef69a9aea7f651ea6ce5b8e24b3242cce
Salih <salih@Salihs-Mac.local>
2024-08-10 18:20:33 +0100

    added dag file to trigger Databricks notbook file

---
+++ 0affec486183_dag.py
@@ -0,0 +1,45 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator, DatabricksRunNowOperator
+from datetime import datetime, timedelta 
+
+
+#Define params for Submit Run Operator
+notebook_task = {
+    'notebook_path': '/Workspace/Users/mr.salih.alborno@gmail.com/Load S3 Topics to DF _ 2024-08-07 12:27:44',
+}
+
+
+#Define params for Run Now Operator
+notebook_params = {
+    "Variable":5
+}
+
+
+default_args = {
+    'owner': 'Salih',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=2)
+}
+
+
+with DAG('0affec486183_dag',
+    # should be a datetime format
+    start_date=datetime(2024, 8, 8),
+    # check out possible intervals, should be a string
+    schedule_interval='@daily',
+    catchup=False,
+    default_args=default_args
+    ) as dag:
+
+
+    opr_submit_run = DatabricksSubmitRunOperator(
+        task_id='submit_run',
+        # the connection we set-up previously
+        databricks_conn_id='databricks_default',
+        existing_cluster_id='1108-162752-8okw8dgg',
+        notebook_task=notebook_task
+    )
+    opr_submit_run
